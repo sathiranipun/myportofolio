cdb2ceedc8d9305bda4db6f55766742d6be6f1ea
sathiranipun <sathiranipun.vsn@gmail.com>
2021-10-04 19:13:09 +0530

    header updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br/>
+        my Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      Experienced Graphic Designer with a demonstrated history of working in the photography industry. Skilled in Photography, Videography, Art, Graphic Design, and Information Technology. Strong media and communication professional with an Honours Degree of Bachelor of Science in Information Technology.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}> Learn More </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
